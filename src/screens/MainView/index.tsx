222da0db5d367a1915a1222ebe9b4d099d50d0c2
ushyDev <uszackip@gmail.com>
2023-10-12 18:58:54 +0200

    added minus button & set config file

--- src/screens/MainView/index.tsx
+++ src/screens/MainView/index.tsx
@@ -2,8 +2,9 @@ import {FC} from 'react';
 import {Text, View} from 'react-native';
 import styles from './styles';
 import {useDispatch, useSelector} from 'react-redux';
-import {RootState, increment} from '../../store';
+import {RootState, decrement, increment} from '../../store';
 import {Button} from '../../components/Button';
+import {MIN_VALUE, MAX_VALUE} from '../../config';
 
 export const MainScreen: FC = () => {
   const counter = useSelector((state: RootState) => state.value);
@@ -13,6 +14,10 @@ export const MainScreen: FC = () => {
     dispatch(increment());
   };
 
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.counterContainer}>
@@ -20,7 +25,12 @@ export const MainScreen: FC = () => {
       </View>
       <View style={styles.buttonsContainer}>
         <Button
-          disabled={counter === 100}
+          disabled={counter === MIN_VALUE}
+          onPress={handleDecrement}
+          title="-"
+        />
+        <Button
+          disabled={counter === MAX_VALUE}
           onPress={handleIncrement}
           title="+"
         />
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit';
+import { MIN_VALUE, MAX_VALUE } from '../config';
 
 const counterSlice = createSlice({
   name: 'counter',
@@ -7,14 +8,19 @@ const counterSlice = createSlice({
   },
   reducers: {
     increment: (state) => {
-      if (state.value < 100) {
+      if (state.value < MAX_VALUE) {
         state.value += 1;
       }
     },
+    decrement: (state) => {
+      if (state.value > MIN_VALUE) {
+        state.value -= 1;
+      }
+    }
   }
 });
 
-export const {increment} = counterSlice.actions;
+export const {increment, decrement} = counterSlice.actions;
 
 export const store = configureStore({
     reducer: counterSlice.reducer
